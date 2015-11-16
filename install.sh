f5f599e8ecb6c11b8ae813dc797b40c143362f3b
yamadapc <tacla.yamada@gmail.com>
2015-11-16 16:48:35 -0200

    Improve the installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
-curl -s -L https://github.com/yamadapc/projectroot/archive/v1.1.2.zip > projectroot.zip
+curl -s -L https://github.com/yamadapc/projectroot/archive/v1.1.2.zip > /tmp/projectroot.zip
+cd /tmp
 unzip projectroot.zip
 cd projectroot-1.1.2
 make
-echo "Copy \`projectroot\` to your PATH"
+make install
+rm -rf /tmp/projectroot.zip
+rm -rf /tmp/projectroot-1.1.2
